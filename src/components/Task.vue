86e89e55b8bdb3d70aa8a75bf750b4e3fc583a70
aaron <schoeffa@gmail.com>
2021-01-03 22:48:19 -0500

    Added slide in for task card

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -5,6 +5,12 @@
   <div class="task-card" v-bind:class="{ 'task-card-open' : open }">
     <span class="material-icons minimize-card" @click="_toggleCard">close</span>
     <div class="task-card-background"></div>
+    <div class="task-data">
+      <p class="task-data-title">{{ task.title }}</p>
+      <p class="task-data-frequency">{{ task.frequency }}</p>
+      <p class="task-data-assignee">{{ task.assignedTo }}</p>
+      <p class="task-data-status">{{ task.status }}</p>
+    </div>
   </div>
 </template>
 
