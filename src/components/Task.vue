9e3216c207aed2a75d38082186d40933d5314725
aaron <schoeffa@gmail.com>
2021-05-26 22:24:44 -0400

    fix card open animation

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,15 +1,15 @@
 <template>
   <input type="checkbox" class="task-checkbox" v-model="open" id="task-checkbox">
-  <label for="task-checkbox"> {{ task.title }}
+  <label for="task-checkbox"> {{ title }}
   </label>
   <div class="task-card" v-bind:class="{ 'task-card-open' : open }">
     <span class="material-icons minimize-card" @click="_toggleCard">close</span>
     <div class="task-card-background"></div>
     <div class="task-data">
-      <p class="task-data-title">{{ task.title }}</p>
-      <p class="task-data-frequency">{{ task.frequency }}</p>
-      <p class="task-data-assignee">{{ task.assignedTo }}</p>
-      <p class="task-data-status">{{ task.status }}</p>
+      <p class="task-data-title">{{ title }}</p>
+      <p class="task-data-frequency">Frequency: {{ frequency }}</p>
+      <p class="task-data-assignee">Assigned To: {{ assignedTo }}</p>
+      <p class="task-data-status">Status: {{ status }}</p>
     </div>
   </div>
 </template>
@@ -27,6 +27,6 @@ export default {
       this.open = !this.open;
     }
   },
-  props: ['task'],
+  props: ['title', 'frequency', 'assignedTo', 'status'],
 }
 </script>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,7 +1,17 @@
 <template>
   <ul>
-    <li v-for="filteredTask in tasks.filter((task) => this.$store.state.user === task.assignedTo && task.status === cat)" :key="filteredTask.title" class="task">
-      <task v-bind:task="filteredTask"></task>
+    <li
+        v-for="filteredTask in tasks.filter((task) => this.$store.state.user === task.assignedTo && task.status === cat)"
+        @click="_setActiveTask(filteredTask)"
+        :key="filteredTask.title"
+        :class="_isActiveTask(filteredTask) ? 'task-active' : ''"
+        class="task"
+    >
+      <task :title="filteredTask.title"
+            :frequency="filteredTask.frequency"
+            :assignedTo="filteredTask.assignedTo"
+            :status="filteredTask.status"
+      ></task>
     </li>
   </ul>
 </template>
@@ -35,9 +45,18 @@ export default {
           assignedTo: 'Aaron',
           status: 'To Do'
         },
-      ]
+      ],
+      activeTask: null
     }
   },
-  props: ['cat']
+  props: ['cat'],
+  methods: {
+    _isActiveTask: function(task) {
+      return task === this.activeTask;
+    },
+    _setActiveTask: function(task) {
+      this.activeTask = task;
+    }
+  }
 }
 </script>
